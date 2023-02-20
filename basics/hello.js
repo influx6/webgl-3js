dd4f93225502452d9b8ce5ae53685d4c175598da
Alexander Ewetumo <trinoxf@gmail.com>
2023-02-20 19:50:36 +0800

    UPT: finished basic scene

--- basics/hello.js
+++ basics/hello.js
@@ -1,1 +1,25 @@
-console.log("hello");
+const t = THREE
+
+const scene = new t.Scene()
+const boxGeometry = new t.BoxGeometry(1, 1, 1)
+const boxMaterial = new t.MeshBasicMaterial({color: 0xff0000})
+const box = new t.Mesh(boxGeometry, boxMaterial)
+
+scene.add(box)
+
+const config = {
+    sizes: {
+        width: 800,
+        height: 600,
+    }
+}
+
+const camera = new t.PerspectiveCamera(75, config.sizes.width / config.sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+const canvas = document.querySelector("canvas#scene")
+const renderer = new t.WebGLRenderer({ canvas, })
+renderer.setSize(config.sizes.width, config.sizes.height)
+
+renderer.render(scene, camera)
